9ec25494671c92bf17c9f3144ea811aa57a8297a
Fadi Naoum <fadi@fadinaoum.de>
2025-10-16 19:20:45 +0200

    WIP: Observable and ObSubscribe

---
+++ src/main/java/de/fadinaoum/obs_to_op/observable/OnSubscribe.java
@@ -0,0 +1,9 @@
+package de.fadinaoum.obs_to_op.observable;
+
+import de.fadinaoum.obs_to_op.Subscription;
+import de.fadinaoum.obs_to_op.observer.Observer;
+
+@FunctionalInterface
+public interface OnSubscribe {
+	<T> Subscription call(Observer<? super T> observer);
+}
