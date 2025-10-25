a52540ee9bfc1712b759f936b6019ea61c2e0f60
Fadi Naoum <fadi@fadinaoum.de>
2025-10-25 12:17:10 +0200

    Fix generics and remove List of subscriptions

--- src/main/java/de/fadinaoum/obs_to_op/observable/OnSubscribe.java
+++ src/main/java/de/fadinaoum/obs_to_op/observable/OnSubscribe.java
@@ -4,6 +4,6 @@ import de.fadinaoum.obs_to_op.Subscription;
 import de.fadinaoum.obs_to_op.observer.Observer;
 
 @FunctionalInterface
-public interface OnSubscribe {
-	<T> Subscription call(Observer<? super T> observer);
+public interface OnSubscribe<T> {
+	Subscription call(Observer<? super T> observer);
 }
